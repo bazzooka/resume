f2d3e38b58cdf77b78458a6274e0374a5a6153b4
Joe <bazzooka@gmail.com>
2015-11-20 13:36:10 +0100

    hand draw of grue :(

--- src/steps/experienceStep.js
+++ src/steps/experienceStep.js
@@ -11,19 +11,22 @@ let ExperienceStep = function(game, layer, addPositionCallback, groups){
     for(var i = 0; i < this.experienceLimit; i++){
         this.createExperience(Experiences[i], i);
     }
+
+    this.drawGrue();
 }
 
 ExperienceStep.prototype.createExperience = function(experience, index){
-    let position = {x: Positions.flyRegion.x1 - 4.5 * Const.TILE_SIZE, y: (Const.GROUND - Const.TILE_SIZE * 6) - (index * 4) * Const.TILE_SIZE  },
+    let position = {x: Positions.flyRegion.x1 - 4.5 * Const.TILE_SIZE, y: (Const.GROUND - Const.TILE_SIZE * 6) - (index * 3) * Const.TILE_SIZE  },
     background_1 = this.game.add.graphics(position.x, position.y),
     background_2 = this.game.add.graphics(position.x, position.y),
-    background_link = this.game.add.graphics(position.x, position.y);
+    background_link = this.game.add.graphics(position.x, position.y),
+    triangle = this.game.add.graphics(position.x, position.y);
 
     this.layer.add(background_1);
     this.layer.add(background_2);
     this.layer.add(background_link);
     
-    let dates = this.game.add.text(position.x + 20, position.y + 20, experience.start + " - " + experience.end);
+    let dates = this.game.add.text(position.x + 20, position.y + 20, experience.start + (experience.end ? " - " + experience.end : ""));
     dates.font = 'Righteous';
     dates.fontSize = 25;
     dates.fill = "#FFFFFF";
@@ -38,14 +41,15 @@ ExperienceStep.prototype.createExperience = function(experience, index){
     role.fontSize = 25;
     role.fill = "#FFFFFF";
 
-    let link = this.game.add.text(position.x + 20, position.y + 150, "Click to launch");
-    link.font = 'Righteous';
-    link.fontSize = 20;
+    let link = this.game.add.text(position.x + 25, position.y + 150, "Click to launch");
+    link.font = 'Reenie Beanie';
+    link.fontSize = 25;
     link.fill = "#FFFFFF";
     link.inputEnabled = true;
-    link.events.onInputOver.add(() => {
-        console.log(experience.link);
+    link.events.onInputDown.add(() => {
+        window.open(experience.link);
     }, this);
+    link.input.useHandCursor = true;
 
 
     background_1.beginFill(0x808080);
@@ -57,8 +61,75 @@ ExperienceStep.prototype.createExperience = function(experience, index){
     background_2.endFill();
 
     background_link.beginFill(0xf58500);
-    background_link.drawRect(10, 150, 200, 30);
+    background_link.drawRect(10, 150, 185, 30);
     background_link.endFill();
+
+    let trianglePosition = {x: link.getLocalBounds().width + 40, y:157 };
+    triangle.beginFill(0xFFFFFF);
+    triangle.moveTo(trianglePosition.x, trianglePosition.y);
+    triangle.lineTo(trianglePosition.x + 10, trianglePosition.y + 7);
+    triangle.lineTo(trianglePosition.x, trianglePosition.y + 14);
+    triangle.endFill();
+
+    background_link.inputEnabled = true;
+    background_link.events.onInputDown.add(() => {
+        window.open(experience.link);
+    }, this);
+
+    background_link.input.useHandCursor = true;
 }
+
+ExperienceStep.prototype.drawGrue = function(){
+    // Freely inspired by http://fr.freepik.com/vecteurs-libre/chantier-de-construction_795668.htm
+    let position = {x: Positions.flyRegion.x2 + 2 * Const.TILE_SIZE, y: (Const.GROUND - Const.TILE_SIZE * 23)   },
+    motifColumn = this.game.add.graphics(position.x, position.y),
+    motifTransvers = this.game.add.graphics(position.x, position.y),
+
+    columnPoints = [],
+    transversPoints = [],
+    columnNbPoints = 10,
+    transversNbPoints = 30,
+    height_ = -40,
+    width_ = 50;
+
+    // Draw column
+    for(let i = 0; i < columnNbPoints; i ++){
+        let x = i%2 === 0 ? 0 : width_;
+        let y = i * height_;
+        columnPoints.push(x);
+        columnPoints.push(y);
+    }
+
+    columnPoints.push(-2, (columnNbPoints-1) * height_);
+    columnPoints.push(-2, 0);
+    columnPoints.push(width_ + 5, 0);
+    columnPoints.push(width_ + 5, (columnNbPoints-1) * height_ - 2);
+
+    motifColumn.lineStyle(4, 0xffd900);
+    motifColumn.drawPolygon(columnPoints);
+
+    // Draw transvers
+    for(let i = 0; i < transversNbPoints; i ++){
+        let x = i%2 === 0 ? 0 : width_;
+        let y = i * height_;
+        transversPoints.push(x);
+        transversPoints.push(y);
+    }
+
+    transversPoints.push(-2, (transversNbPoints-1) * height_);
+    transversPoints.push(-2, 0);
+    transversPoints.push(width_ + 5, 30);
+    transversPoints.push(width_ + 5, (transversNbPoints-1) * height_ - 2);
+
+    motifTransvers.lineStyle(4, 0xffd900);
+    motifTransvers.drawPolygon(transversPoints);
+    motifTransvers.angle = 90;
+    motifTransvers.y += (columnNbPoints) * height_ - 15;
+    motifTransvers.x -= motifTransvers.height - Const.TILE_SIZE * 2;
+
+
+    // Draw transverse
+
+};
 
 export default ExperienceStep;
